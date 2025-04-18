8a5d80570b59a12cc91c4e523aba0ca7149db40c
Sabit Bekturov <175160300+HailReese@users.noreply.github.com>
2025-04-19 03:11:28 +0600

    solve Task1 in day2

--- src/day2/Task1.java
+++ src/day2/Task1.java
@@ -1,16 +1,31 @@
 package day2;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Task1 {
 	public static void main(String[] args) {
-		System.out.print("Введи сколько этажей в доме: ");
+		System.out.print("Введите количество этажей в доме: ");
 		int floors = 0;
+
 		try (Scanner sc = new Scanner(System.in)) {
 			floors = sc.nextInt();
-		} catch (Exception e) {
-			System.out.println("Вы ввели неверное число");
-			return;
+
+			if (floors <= 0) {
+				System.out.println("Ошибка ввода");
+				return;
+			}
+
+			if (floors <= 4) {
+				System.out.println("Малоэтажный дом");
+			} else if (floors <= 8) {
+				System.out.println("Среднеэтажный дом");
+			} else {
+				System.out.println("Многоэтажный дом");
+			}
+
+		} catch (InputMismatchException e) {
+			System.out.println("Ошибка ввода: введите целое число");
 		}
 	}
 }
