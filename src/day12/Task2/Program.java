f03f4fbfdf31b972f677abed31538a6f9fd3d147
Sabit Bekturov <175160300+HailReese@users.noreply.github.com>
2025-04-24 00:16:48 +0600

    solve Task2 in day12

---
+++ src/day12/Task2/Program.java
@@ -0,0 +1,19 @@
+package day12.Task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+
+		for (int i = 0; i <= 350; i++) {
+			if (i == 31)
+				i = 300;
+			if (i % 2 == 0) {
+				list.add(i);
+			}
+		}
+		System.out.println(list.toString());
+	}
+}
